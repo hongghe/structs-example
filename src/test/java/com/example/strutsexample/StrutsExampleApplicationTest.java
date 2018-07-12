a2ba43066d2d8c9b93f93e12251d2c2c733e3d45
hongghe <364930855@qq.com>
2018-07-12 18:30:13 +0800

    Rename file.

--- src/test/java/com/example/strutsexample/StrutsExampleApplicationTest.java
+++ src/test/java/com/example/strutsexample/StrutsExampleApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.structsexample;
+package com.example.strutsexample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StructsExampleApplicationTests {
+public class StrutsExampleApplicationTest {
 
 	@Test
 	public void contextLoads() {
