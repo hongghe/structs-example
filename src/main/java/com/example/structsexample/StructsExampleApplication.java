a443d6194dc2f200b6f7b0f72fe9f09d4fa7968b
hongghe <364930855@qq.com>
2018-07-12 18:19:54 +0800

    Initial commit

---
+++ src/main/java/com/example/structsexample/StructsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.structsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructsExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/structsexample/StructsExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.structsexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StructsExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
